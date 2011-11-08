78f47a6673e5f5a1cbe391c3cd77c07cf2801f04
Anatoly Kanivetsky <chaos-ad@yandex.ru>
2011-11-09 02:39:49 +0400

    Visual studio build + trade safety

--- main.cpp
+++ main.cpp
@@ -2,12 +2,18 @@
 
 #include <iostream>
 #include <boost/bind.hpp>
+#include <boost/thread/locks.hpp>
+#include <boost/thread/mutex.hpp>
 
+
+boost::mutex mutex;
 
 // called from different thread, but, who cares
 bool handle_data(std::string const& data)
 {
+    boost::lock_guard<boost::mutex> lock(mutex);
     std::cout << data << std::endl;
+    return true;
 }
 
 
@@ -18,7 +24,9 @@ int main(int argc, char **argv)
     {
         std::string command;
         std::getline(std::cin, command);
-        transaq::wrapper::send_command(command);
+        std::string result = transaq::wrapper::send_command(command);
+        boost::lock_guard<boost::mutex> lock(mutex);
+        std::cout << result << std::endl;
     }
 
     transaq::wrapper::stop();
