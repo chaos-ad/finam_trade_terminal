1c2c24a2aea0c38d9530968f617756251da4c9af
Anatoly Kanivetsky <chaos-ad@yandex.ru>
2011-11-09 02:15:20 +0400

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
-#include <iostream>
+#include "transaq_wrapper.h"
 
-int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
+#include <iostream>
+#include <boost/bind.hpp>
+
+
+// called from different thread, but, who cares
+bool handle_data(std::string const& data)
+{
+    std::cout << data << std::endl;
+}
+
+
+int main(int argc, char **argv)
+{
+    transaq::wrapper::start(boost::bind(handle_data, _1), "txmlconnector.dll");
+    for(;;)
+    {
+        std::string command;
+        std::getline(std::cin, command);
+        transaq::wrapper::send_command(command);
+    }
+
+    transaq::wrapper::stop();
     return 0;
 }
